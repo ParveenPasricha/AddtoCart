dd3632c2ffc23267c7bde2799cdab058a7ec10df
ParveenPasricha <pasricha.parveen@yahoo.in>
2025-02-04 23:30:03 +0530

    Style: Styling updated

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removecart } from "../Store/Slice";
 
 const Cart = () => {
-  const cartItems = useSelector(state => state.Cart.cartItems);
+  const dispatch = useDispatch();
+  const handleRemove = (id) => {
+    console.log("Remove Item", id);
+    dispatch(removecart({ id }));
+  };
+
+  const cartItems = useSelector((state) => state.Cart.cartItems);
 
   return (
-        <div className="cart-container flex">
-          {cartItems.length === 0 ? (
-            <p>Your cart is empty</p>
-          ) : (
-            <div className='ml-48 bg-black text-white shadow-2xl rounded-3xl p-5'>
-              {cartItems.map((item) => (
-                <div key={item.id} className="text-center ">
-                  <div className='flex items-center'>
-                  <img src={item.image} alt={item.title} className='w-20 justify-center' />
-                  <p className='m-5'>{item.title}</p>
-                  <p>Price: Rs {item.price}/- </p>
-                  <p className='ml-2'>Quantity: {item.quantity}</p>
-                  </div>
-                </div>
-              ))}
+    <div className="cart-container flex">
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div className="ml-48 bg-black text-white shadow-2xl rounded-3xl p-5">
+          {cartItems.map((item) => (
+            <div key={item.id} className="text-center ">
+              <div className="flex items-center">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="w-20 justify-center"
+                />
+                <p className="m-5">{item.title}</p>
+                <p>Price: Rs {item.price}/- </p>
+                <p className="ml-2">Quantity: {item.quantity}</p>
+                <br />
+                <button
+                  className="text-white font-bold bg-gradient-to-r from-blue-700 to-grey-700 text-2xl rounded-lg"
+                  onClick={()=>handleRemove(item.id)}
+                >
+                  Remove
+                </button>
+              </div>
             </div>
-          )}
+          ))}
         </div>
-      );
-    };
-    
-    export default Cart;
-    
+      )}
+    </div>
+  );
+};
+
+export default Cart;
--- src/Store/Slice.js
+++ src/Store/Slice.js
@@ -15,9 +15,22 @@ const Slice = createSlice({
             else{
                 state.cartItems.push({...action.payload, quantity : 1})
             }
+        },
+        removecart:(state, action)=>{
+            const existingItem= state.cartItems.find(item => item.id === action.payload.id)
+            if(existingItem.quantity > 1){
+                existingItem.quantity -= 1
+            }
+            else if(existingItem.quantity === 0)
+            {
+                state.cartItems.remove(item => item.id !== action.payload.id)
+            }
+            else{
+                state.cartItems = state.cartItems.filter(item => item.id !== action.payload.id)
+            }
         }
     }
 })
-export const {addtocart} = Slice.actions
+export const {addtocart, removecart} = Slice.actions
 export default Slice.reducer
 
